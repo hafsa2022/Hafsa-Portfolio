4c99d07336bf5f261b61f62497da40c31d4714b8
hafsa2022 <elakhdarhafsa@gmail.com>
2025-12-14 17:27:50 +0100

    commit 4

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
